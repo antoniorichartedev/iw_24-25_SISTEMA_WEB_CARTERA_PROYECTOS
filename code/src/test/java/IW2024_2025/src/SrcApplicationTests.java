d2f18e6c9eb5e217b77ef895f358c9503461040f
Pablo Garcia <pablocns2002@gmail.com>
2024-10-16 10:35:18 +0200

    V0.0
    Versión inicial del proyecto.

---
+++ code/src/test/java/IW2024_2025/src/SrcApplicationTests.java
@@ -0,0 +1,13 @@
+package IW2024_2025.src;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
