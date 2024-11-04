89a53b4d7a7972c28f36e246ebfda42e0b29dd90
lauguerrero <lauguera2002@gmail.com>
2024-11-04 17:17:29 +0100

    clase solicitante

---
+++ src/main/java/src/spring/Solicitante/Solicitante.java
@@ -0,0 +1,31 @@
+package src.spring.Solicitante;
+package src.spring.Persona;
+
+import jakarta.persistence.Entity;
+
+public class Solicitante extends Persona{
+    private String correo;
+    private String unidadSolicitante;
+
+    public Solicitante(String nombre, String correo, String unidadSolicitante){
+        this.nombre = nombre;
+        this.correo = correo;
+        this.unidadSolicitante = unidadSolicitante;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getUnidadSolicitante() {
+        return unidadSolicitante;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setUnidadSolicitante(String unidadSolicitante) {
+        this.unidadSolicitante = unidadSolicitante;
+    }
+}
