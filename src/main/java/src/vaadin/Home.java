95bf4418c98e68cba9972963720dd9373f2533fd
Pablo Garcia <pablocns2002@gmail.com>
2024-11-04 17:47:32 +0100

    vaadin
    Añadida una clase de prueba de vaadin

---
+++ src/main/java/src/vaadin/Home.java
@@ -0,0 +1,16 @@
+package src.vaadin;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+@Route("")
+public class Home extends VerticalLayout {
+
+    public Home() {
+        Button button = new Button("Click me",
+                event -> Notification.show("¡Hola desde Vaadin y Spring Boot!"));
+        add(button);
+    }
+}
